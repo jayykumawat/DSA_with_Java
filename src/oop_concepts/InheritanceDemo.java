606f022ab05dd18891ceefc8e8317ca6c4a4e9d5
jayykumawat <kumawatjayy1133@gmail.com>
2026-02-18 22:57:08 +0530

    Day 4: OOP concepts with code (inheritance)

---
+++ src/oop_concepts/InheritanceDemo.java
@@ -0,0 +1,40 @@
+package oop_concepts;
+
+/*
+Problem:
+Demonstrate inheritance and method overriding in Java.
+
+Approach:
+- Create base class Animal with sound()
+- Create child class Dog that overrides sound()
+- Call methods and observe runtime behavior
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.sound();
+
+        Dog d = new Dog();
+        d.sound();
+
+        // Polymorphism example:
+        Animal ref = new Dog();
+        ref.sound();
+    }
+}
