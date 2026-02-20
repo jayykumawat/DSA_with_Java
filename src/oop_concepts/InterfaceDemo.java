070411d3399d5f6bff83fab622b633eb180e2f6c
jayykumawat <kumawatjayy1133@gmail.com>
2026-02-20 23:45:45 +0530

    Day 6: OOP concepts (interface)

--- src/oop_concepts/InterfaceDemo.java
+++ src/oop_concepts/InterfaceDemo.java
@@ -25,6 +25,7 @@ class UpiPayment implements Payment {
 
 public class InterfaceDemo {
     public static void main(String[] args) {
+        
         Payment payment = new UpiPayment();
         payment.pay(500);
     }
