9651f646872621f3d8e9dd734592e98b288ebe4d
jayykumawat <kumawatjayy1133@gmail.com>
2026-03-02 00:08:42 +0530

    some correction in EncapsulationTest file

--- src/oops_Levelup/encapsulation/EncapsulationTest.java
+++ src/oops_Levelup/encapsulation/EncapsulationTest.java
@@ -3,8 +3,8 @@ package oops_Levelup.encapsulation;
 public class EncapsulationTest {
     public static void main(String[] args) {
         
-        BankAccount acc1=new BankAccount("Jay", 10000f);
-        BankAccount acc2=new BankAccount("Vidhan", 5000f);
+        BankAccount acc1=new BankAccount("Jay", 10000);
+        BankAccount acc2=new BankAccount("Vidhan", 5000);
   
     BankService service = new BankService();
     
@@ -13,7 +13,7 @@ public class EncapsulationTest {
     System.out.println(acc1.getAccountHolder()+" balance is "+acc1.getBalance());
     System.out.println(acc2.getAccountHolder()+" balance is "+acc2.getBalance());
     
-    service.Transfer(acc1,acc2,2000f);
+    service.Transfer(acc1,acc2,2000);
     //after transfer the amount we check the balance 
     System.out.println(acc1.getAccountHolder()+" balance is "+acc1.getBalance());
     System.out.println(acc2.getAccountHolder()+" balance is "+acc2.getBalance());
