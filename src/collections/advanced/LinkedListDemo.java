6fd9cde63e19aedccd1963fc15e852047f31d877
jayykumawat <kumawatjayy1133@gmail.com>
2026-03-05 15:26:09 +0530

    Phase 3_Day-20 :LinkedList operations(doubly LL) demonstration

---
+++ src/collections/advanced/LinkedListDemo.java
@@ -0,0 +1,35 @@
+package collections.advanced;
+import java.util.LinkedList;
+
+/**
+ * Demonstrates basic operations of LinkedList.
+ *
+ * LinkedList internally uses a doubly linked list.
+ *
+ * Time Complexity:
+ * Insert/Delete at ends → O(1)
+ * Random access → O(n)
+ */
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        
+        LinkedList<String>cities=new LinkedList<>();
+        cities.add("Mumbai");
+        cities.add("Delhi");
+        cities.add("Bangalore");
+
+        cities.addFirst("Kolkata");
+        cities.addLast("Chennai");
+
+         System.out.println("Cities: " + cities);
+
+        cities.removeFirst();
+        cities.removeLast();
+
+        System.out.println("After removals: " + cities);
+
+        System.out.println("First city: " + cities.getFirst());
+        System.out.println("Last city: " + cities.getLast());
+    }
+    
+}
