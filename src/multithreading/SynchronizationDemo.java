6d4b932a323ffb159b59bc87b0164d01207e47b6
jayykumawat <kumawatjayy1133@gmail.com>
2026-03-10 12:05:09 +0530

    Phase: Synchronization and race condition handling

---
+++ src/multithreading/SynchronizationDemo.java
@@ -0,0 +1,44 @@
+package multithreading;
+
+/**
+ * Demonstrates synchronization to avoid race condition.
+ */
+class Counter{
+    private int count=0;
+    public synchronized void Increment(){
+        count++;
+    }
+    public int getCount(){
+        return count;
+    }
+}
+class CounterThread extends Thread{
+   
+    Counter counter;
+    CounterThread(Counter counter){
+        this.counter=counter;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.Increment();
+        }
+    }
+}
+
+public class SynchronizationDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter=new Counter();
+       
+        CounterThread t1=new CounterThread(counter);
+        CounterThread t2=new CounterThread(counter);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Counter Value: "+counter.getCount());
+    }
+}
