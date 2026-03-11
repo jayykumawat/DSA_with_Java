57fe748ea64b9a57afb865add3a98d321ab0f7ff
jayykumawat <kumawatjayy1133@gmail.com>
2026-03-11 11:45:39 +0530

    Day-26_Phase 4: Deadlock scenario demonstration

---
+++ src/multithreading/DeadlockDemo.java
@@ -0,0 +1,49 @@
+package multithreading;
+
+/**
+ * Demonstrates Deadlock situation.
+ */
+
+class ResourceA {}
+
+class ResourceB {}
+
+public class DeadlockDemo {
+
+    public static void main(String[] args) {
+
+        ResourceA resA = new ResourceA();
+        ResourceB resB = new ResourceB();
+
+        Thread t1 = new Thread(() -> {
+
+            synchronized (resA) {
+
+                System.out.println("Thread 1 locked Resource A");
+
+                try { Thread.sleep(100); } catch (Exception e) {}
+
+                synchronized (resB) {
+                    System.out.println("Thread 1 locked Resource B");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+
+            synchronized (resB) {
+
+                System.out.println("Thread 2 locked Resource B");
+
+                try { Thread.sleep(100); } catch (Exception e) {}
+
+                synchronized (resA) {
+                    System.out.println("Thread 2 locked Resource A");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
