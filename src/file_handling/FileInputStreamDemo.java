9f15000522482f79e39822d35b493ca9cf34e785
jayykumawat <kumawatjayy1133@gmail.com>
2026-03-14 16:07:39 +0530

    Day-29_Phase 5: FileInputStream byte streams

---
+++ src/file_handling/FileInputStreamDemo.java
@@ -0,0 +1,19 @@
+package file_handling;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamDemo {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("data/test.txt");
+            int data;
+            while((data=fis.read())!=-1){
+                System.out.print((char)data);
+            }
+            fis.close();
+        } catch (IOException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
