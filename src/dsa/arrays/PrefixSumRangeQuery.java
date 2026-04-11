de863d31c3c90dc4cb02dff57b069bf2a810f675
jayykumawat <kumawatjayy1133@gmail.com>
2026-04-11 13:43:43 +0530

    DSA :Prefix Sum

--- src/dsa/arrays/PrefixSumRangeQuery.java
+++ src/dsa/arrays/PrefixSumRangeQuery.java
@@ -1,5 +1,28 @@
 package dsa.arrays;
 
+import java.util.Arrays;
+
 public class PrefixSumRangeQuery {
-    
+    public static int[]buildPrefix(int[]arr){
+        int[]prefix=new int[arr.length];
+        prefix[0]=arr[0];
+        for(int i=1;i<arr.length;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+        return prefix;
+    }
+    public static int rangeSum(int[]prefix,int R,int L){
+        if(L==0)return prefix[R];
+        return prefix[R]-prefix[L-1];
+    }
+    public static void main(String[] args) {
+        int[]arr={2,4,6,8,10};
+        System.out.println("Original Array "+Arrays.toString(arr));
+        int[]prefix=buildPrefix(arr);
+        System.out.println("Prefix Array: "+Arrays.toString(prefix));
+        
+        int l=0,r=3;
+        System.out.println("Range from index "+l+" to "+r);
+        System.out.println("Range Sum: "+rangeSum(prefix, r, l));
+    }
 }
