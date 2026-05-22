caad5ddafaec011aa9afc28a2f754b5df3178ab0
jayykumawat <kumawatjayy1133@gmail.com>
2026-05-22 20:01:35 +0530

    DSA Strings: Palindrome Check program

--- src/dsa/strings/ValidPalindrome.java
+++ src/dsa/strings/ValidPalindrome.java
@@ -1,5 +1,28 @@
 package dsa.strings;
 
+import java.util.Scanner;
+
 public class ValidPalindrome {
-    
+    static boolean ispalindrome(String str){
+      int left=0;
+      int right=str.length()-1;
+
+      while (left<right) {
+        if(str.charAt(left)!=str.charAt(right)){
+            return false;
+        }
+        left++;
+        right--;
+      }
+      return true;
+    }
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a String to check is it palindrome or not");
+       
+        String str=sc.nextLine().toLowerCase();
+        System.out.println("IS Palindrome: "+ispalindrome(str));
+
+        sc.close();
+    }
 }
