c5310f4980cbbb5766ed3620b1bc14b61e91ffae
jayykumawat <kumawatjayy1133@gmail.com>
2026-05-22 20:02:13 +0530

    DSA Strings: Anagram Check Program

--- src/dsa/strings/AnagramCheck.java
+++ src/dsa/strings/AnagramCheck.java
@@ -1,5 +1,26 @@
 package dsa.strings;
 
+import java.util.Arrays;
+
 public class AnagramCheck {
-    
+    static boolean isanagram(String s1,String s2){
+         
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+
+        char[]arr1=s1.toCharArray();
+        char[]arr2=s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+    public static void main(String[] args) {
+        String s1="jay";
+        String s2="sawan";
+
+        System.out.println("Check two string is anagram: "+isanagram(s1, s2));
+    }
 }
