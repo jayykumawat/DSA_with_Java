c7a586a2523b17c0a41dff15e3f5f0066a389174
jayykumawat <kumawatjayy1133@gmail.com>
2026-05-24 23:42:52 +0530

    DSA Strings:Longest Common Prefix

--- src/dsa/strings/LongestCommonPrefix.java
+++ src/dsa/strings/LongestCommonPrefix.java
@@ -1,5 +1,45 @@
 package dsa.strings;
 
+/*
+PROBLEM:
+Find longest common prefix among strings.
+
+APPROACH:
+Compare characters one-by-one.
+
+TIME: O(n * m)
+SPACE: O(1)
+*/
+
 public class LongestCommonPrefix {
-    
+
+    public static String longestPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (strs[i].indexOf(prefix) != 0) {
+
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"flower", "flow", "flight"};
+
+        System.out.println("Longest Prefix: " + longestPrefix(strs));
+    }
 }
