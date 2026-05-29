be8ac52daca0b11916c3cad96a9a5e459d231495
jayykumawat <kumawatjayy1133@gmail.com>
2026-05-29 23:52:11 +0530

    Solving TwoSum in better_approach

--- src/dsa/arrays/TwoSum.java
+++ src/dsa/arrays/TwoSum.java
@@ -11,18 +11,26 @@ TIME: O(n)
 SPACE: O(n)
 */
 import java.util.Arrays;
-import java.util.HashMap;
+
 
 public class TwoSum {
     public static int[]calculate(int[]arr,int target){
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for (int i = 0; i <arr.length ; i++) {
-            int complement=target-arr[i];
-            if(map.containsKey(complement)){
-                return new int[]{map.get(complement),i};
-            }
-            map.put(arr[i],i);
-        }
+       int left =0;
+       int right=arr.length-1;
+       int sum=0;
+       while(left<right){
+           sum=arr[left]+arr[right];
+           
+           if(sum==target){
+               return new int[]{left+1,right+1};
+           }
+           else if(sum<target){
+               left++;
+           }
+           else{
+               right--;
+           }
+       }
         return new int[]{-1,-1};
     }
     static void main(String[] args) {
