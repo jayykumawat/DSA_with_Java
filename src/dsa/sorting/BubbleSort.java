5a5f47f6c1289656e56764591ca1031e0c87baae
jayykumawat <kumawatjayy1133@gmail.com>
2026-07-10 12:17:16 +0530

    BubbleSort program

--- src/dsa/sorting/BubbleSort.java
+++ src/dsa/sorting/BubbleSort.java
@@ -1,5 +1,29 @@
 package dsa.sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
-    
+    static void sort(int[]arr){
+        boolean Swapped;
+        for(int i=0;i<arr.length;i++){
+            Swapped=false;
+            for (int j = 0; j < arr.length-1-i; j++) {
+                
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+                Swapped=true;
+            }
+            if(!Swapped){
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[]arr={7,4,6,2,3,1,5};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
